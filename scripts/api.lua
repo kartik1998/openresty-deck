d2da74186087e00278ef1e9ca3f8f931cf68b174
kartikr <kartikr@adobe.com>
2024-03-19 11:03:31 +0530

    read the body

--- scripts/api.lua
+++ scripts/api.lua
@@ -1,6 +1,11 @@
+local ngx = ngx
+
 function respond(reason, status_code)
+    ngx.log(ngx.INFO, "respond method entered")
+    ngx.req.read_body()
+    local body = ngx.req.get_body_data()
     ngx.status = status_code
-    ngx.say(reason)
+    ngx.say(body)
 end
 
 return respond("hi, you hit the api endpoint", 203)
